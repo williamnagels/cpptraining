a969a903ce27c08628e4b585d1c9234f12e1b02f
William <william.nagels@gmail.com>
2024-09-09 18:12:38 +0200

    Update ex7.cpp

--- exercises/containers/ex7.cpp
+++ exercises/containers/ex7.cpp
@@ -3,6 +3,19 @@
 namespace
 {
 /*
+GOAL:
+The goal of this excercise is to implement std::array and show how easy it is to expose access to internal data
+with a custom iterator.
+*/
+
+/*
+TODO: Uncomment the class 'Array' below, implement the iterator methods of the nested Iterator type 
+in a way that the Iterator class behaves like an actual random access iterator.
+Add some kind of member variable to the Iterator class to be able to store a reference to an instance of 
+type T stored in the Array<T, SIZE> . Type T is exposed by dereferencing the Iterator type.
+*/
+
+/*
 template <typename T, std::size_t SIZE>
 class Array
 {
@@ -20,8 +33,8 @@ public:
 
         reference operator*() const { }
         pointer operator->() {}
-        Iterator& operator++() { }  
-        Iterator operator++(int) {}
+        Iterator& operator++() { }  //prefix operator overload
+        Iterator operator++(int) {} //postfix operator overload
         friend bool operator== (const Iterator& a, const Iterator& b) { };
         friend bool operator!= (const Iterator& a, const Iterator& b) { };  
 
@@ -31,16 +44,18 @@ public:
     Iterator begin() { }
     Iterator end()   { }
 };*/
-//It is possible to create custom iterators for a type.
-//Lets create our own std::array. and put the first 100 fibonacci nrs as values by using std::generate
-//and our own iterator.
-//a blueprint for a custom array type with own iterator is provided above. 
-//Forget about the leading zero of that series. or dont. but then you have to change the assert to take that into account
+
 void test_1()
 {
-//    Array<int, 100> test;
-//   ASSERT(test[0] == 1);
-//   ASSERT(test[4] == 8);
+    //TODO: uncomment the Array instanziation
+    // Array<int, 100> test;
+    
+    //TODO: Add code to assign the values of the first 100 fibonnaci numbers to the array, use std::generate
+    
+    //TODO:uncomment the asserts below, this asssert assumes fibonnaci starts with 1, 
+    //if you added the leading zero's change the asser.
+    // ASSERT(test[0] == 1);
+    // ASSERT(test[4] == 8);
 }
 }
 
