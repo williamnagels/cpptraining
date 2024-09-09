dffa6a25d709e0b0b8f72fc15dbeb79d324feda1
William <william.nagels@gmail.com>
2024-09-09 18:55:40 +0200

    Update ex5.cpp

--- exercises/containers/ex5.cpp
+++ exercises/containers/ex5.cpp
@@ -3,7 +3,9 @@
 namespace
 {
 
-/* Update AddElement; return true if new element added, else return false
+/* Function addElement; 
+returns true if an elemente was added to container 'v', false if the element was already in the container 'v'
+TODO:Fix the behaviour when working on test_1
 */
 template<typename T>
 bool addElement(T& v, int valueToAdd){
@@ -11,30 +13,46 @@ bool addElement(T& v, int valueToAdd){
   return true;
 }
 
-//Container 'v' must contain unique numbers only. The original author created an API : addElement
-//to add elements to a container and check if they can be added (are unique) before adding.
-//Is std::vector the best container for the job? If using any ascociative container,
-//std::unique may no longer compile and the assert has to commented out, why is that?
+/*
+GOAL: Ascociative containers can implicitly provide sorting behaviour which would otherwise have to be fixed explicitly by 
+invoking an std::algorithm.
+Container 'v' must always only contain unique numbers, duplicates are not allowed.
+The original author created a preliminary API : addElement which does provide this guarantee.
+Either change the implementation of addElement to detect a duplicate or change the container type of 'v'
+*/
 void test_1()
 {
-  std::vector<int> v = {1,2 ,7, 8 ,9 ,10};
+  std::vector<int> v = {1, 2, 7, 8, 9, 10};
 
   ASSERT(addElement(v, 4) == true); 
+  //TODO:uncomment the call to addElement after the addElement implementation has been fixed.
   //ASSERT(addElement(v, 8) == false);
   ASSERT(std::unique(std::begin(v), std::end(v)) ==std::end(v));
 }
-//Start sorting the container in ascending order, but stop after 4 elements have been sorted.
-//You may assume that the original vector v has at least four elements.
+/*
+GOAL: Sort the container 'v' in ascending order. Stop sorting after 4 elements have been sorted.
+Do not assume that v has at minimum 4 elements.
+*/
 void test_2()
 {
-  std::vector<int> v = {10,3,8,6,4,1,5};
-
+  std::vector<int> v = {10, 3, 8, 6, 4, 1, 5};
+  //TODO: add sort std algorithm invoke.
+  
+  //TODO: uncomment the asserts below
   /*ASSERT(v[0] == 1);
   ASSERT(v[1] == 3);
   ASSERT(v[2] == 4);
   ASSERT(v[3] == 5);*/
   ASSERT(std::is_sorted(std::begin(v), std::end(v)) == false);
 
+  v = {3, 4, 1};
+  //TODO: reinvoke the sort function to sort container 'v'. 
+  //Check that it also works on containers with less than 4 integer instances.
+  
+  /*ASSERT(v[0] == 1);
+  ASSERT(v[1] == 3);
+  ASSERT(v[2] == 4);*/
+  ASSERT(std::is_sorted(std::begin(v), std::end(v)) == true);
 }
 }
 
