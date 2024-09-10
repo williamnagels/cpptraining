7968a7239089e7283c64b6e6340ead6dd1e2a52b
William <william.nagels@gmail.com>
2024-09-10 18:51:17 +0200

    Update main.cpp

--- exercises/referencesemantics/main.cpp
+++ exercises/referencesemantics/main.cpp
@@ -1,21 +1,5 @@
 #include <global.h>
 
-class LargeObject
-{
-public:
-  LargeObject() { ++s_constructed; }
-  LargeObject(const LargeObject &) { ++s_constructed; }
-  LargeObject &operator=(const LargeObject &) { ++s_copied; return *this; }
-
-  static uint64_t s_constructed;
-  static uint64_t s_copied;
-  static uint64_t s_moved;
-};
-
-uint64_t LargeObject::s_constructed = 0;
-uint64_t LargeObject::s_copied = 0;
-uint64_t LargeObject::s_moved = 0;
-
 void move_ex1();
 void move_ex2();
 void move_ex3();
