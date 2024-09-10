029c4ff19a92a8d36c6545472e4b988a9a70930b
William <william.nagels@gmail.com>
2024-09-10 18:22:57 +0200

    Update ex2.cpp

--- exercises/referencesemantics/ex2.cpp
+++ exercises/referencesemantics/ex2.cpp
@@ -3,9 +3,8 @@
 namespace
 {
 
-/**
- * You do not need to modify this object
- */
+// This class does not need to be modified for the asserts in 'test_1' to succeed.
+// Only function 'split' and 'test_1' have to be modified.
 struct Object1
 {
   Object1(uint64_t v) : m_value(v) { ++s_constructs; }
@@ -25,11 +24,11 @@ uint64_t Object1::s_copies = 0;
 
 
 
-/**
- * Uncomment the ASSERTs in "test_1" and 
- * modify the function "split" and "test_1" so the asserts pass
- */
-
+/*
+'split' splits a vector 'vec' passed as argument and returns 2 new vectors.
+One vector contains all Object1 instances which have an 'even' value in data member 'm_value'.
+The other newly created vector contains a vector with all Object1 instances which have an 'uneven' value in data member 'm_value'
+*/
 std::pair<std::vector<Object1>, std::vector<Object1>> split(const std::vector<Object1> vec)
 {
   std::vector<Object1> even;
@@ -43,7 +42,12 @@ std::pair<std::vector<Object1>, std::vector<Object1>> split(const std::vector<Ob
   }
   return {even, uneven};
 }
-
+/* 
+GOAL: To goal of this excercise is to show how move semantics may ellide copies.
+In function 'split' and function 'test_1' there are a lot of expressions that cause copies.
+Not all copies are needed. Investigate which copies can be removed and which copies can be 
+replaced by moves.
+*/
 void test_1()
 {
   std::vector<Object1> v;
@@ -53,25 +57,20 @@ void test_1()
   }
   auto value = split(v);
 
-  // these lines might help you debug
-  
+  // These prints might help you debug:
   std::cout << "constructs: " << Object1::s_constructs << "\n";
   std::cout << "moves: " << Object1::s_moves << "\n";
   std::cout << "copies: " << Object1::s_copies << "\n";
   std::cout << "evens: " << value.first.size() << "\n";
   std::cout << "unevens: " << value.second.size() << "\n";
-  
 
-  // uncomment these line, you do not need to modify them
+  // TODO: uncomment these lines. Fix the code in 'test_1' and 'split' so that the asserts succeed.
   /*ASSERT(Object1::s_constructs == 10);
   ASSERT(Object1::s_moves == 10);
   ASSERT(Object1::s_copies == 0);
   ASSERT(value.first.size() == 5);
   ASSERT(value.second.size() == 5);*/
-  
 }
-
-
 }
 
 void move_ex2()
