17a28e42df007f1c36b81e4a78f021e96848e538
William <william.nagels@gmail.com>
2024-09-10 18:12:34 +0200

    Update ex1.cpp

--- exercises/referencesemantics/ex1.cpp
+++ exercises/referencesemantics/ex1.cpp
@@ -1,48 +1,43 @@
 #include <global.h>
 
-
 namespace
 {
 
-/***
- * Exercise 1
- * Add move constructors to the following object
- */
 struct Object_1
 {
   Object_1(const std::string &v) : m_value(v) {}
   Object_1(const Object_1 &obj) : m_value(obj.m_value) {}
-    
+  //TODO: Implement constructors.
   std::string m_value;
 };
 
-
-/*
- * How do you know that your code works?
- * Add code here to verify that your new constructors work
- * You have an assert macro available:
- *   ASSERT(1 == 1);
+/* GOAL: Get familiar with move semantics by adding a move constructor to the Object_1 class.
+ * Add runtime asserts to verify that the move constructor is invoked.
+ * An ASSERT macro is provided by global.h. Take a look at test_2 for assert demo and inspiration on how to use it.
+ * TIP: a moved from std::string is empty.
  */
 void test_1()
 {
-/*  std::string str = "obj";
-  Object_1 o(std::move(str)); */
+  //TODO: uncomment these lines when the move constructor is implemented
+  /*
+  std::string str = "obj";
+  Object_1 o(std::move(str)); 
+  */
+  //TODO: add assert to verify that move constructor is invoked.
 }
- 
- 
-/*
- * What do you need to also add support for assignment
- * Uncomment the following code and make them work
+/* GOAL: Get familiar with move semantics by add a move assignment constructor to the 
+ * Object_1 class.
  */
 void test_2()
 {
-/*
+  //TODO: uncomment these lines when the move assignment constructor is implemented.
+  /*
   Object_1 obj1("obj1");
   Object_1 obj2("obj2");
   obj1 = std::move(obj2);
   ASSERT(obj2.m_value.size() == 0);
   ASSERT(obj1.m_value == "obj2");
-*/
+  */
 }
 
 } 
