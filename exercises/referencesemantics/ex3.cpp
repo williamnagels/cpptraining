b226e743cbaf663ec877f7f2ee8a293530bf27c7
William <william.nagels@gmail.com>
2024-09-10 18:30:02 +0200

    Update ex3.cpp

--- exercises/referencesemantics/ex3.cpp
+++ exercises/referencesemantics/ex3.cpp
@@ -1,13 +1,10 @@
 #include <global.h>
 
-/**
- * Uncomment the code in "test_1" and change the "builder" function to make it work
- */
-
 
 namespace
 {
 
+// This class should not be changed. It has all the required constructors.
 struct Object
 {
   Object(const std::string &s) : m_value(s) {}
@@ -17,13 +14,17 @@ struct Object
   std::variant<std::string, std::unique_ptr<int>> m_value;
 };
 
-
+//TODO: modify this function either be upgrading the argument to a universal reference or by providing
+//a seperate move only overload.
 Object builder(const std::string &v)
 {
   return Object(v);
 }
 
-
+/* GOAL: Make the builder function work with types that support move semantics.
+ * Either provide a move overload for the 'builder' function or use a universal reference.
+ * The Object class should not be modified.
+ */
 void test_1()
 {
   std::string v1 = "1";
@@ -31,6 +32,7 @@ void test_1()
   ASSERT(std::get<std::string>(obj1.m_value) == "1");
   ASSERT(v1.size() == 1);
 
+  //TODO: uncomment when the builder function has been reimplemented.
   /*
   std::string v2 = "2";
   auto obj2 = builder(std::move(v2));
