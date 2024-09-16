d503e1f4b693cfdb95ee40e7d3b6406ee6d42e51
William <william.nagels@gmail.com>
2024-09-16 14:51:54 +0200

    Update ex2.cpp

--- exercises/valuesemantics/ex2.cpp
+++ exercises/valuesemantics/ex2.cpp
@@ -5,23 +5,35 @@ namespace
 
 struct Vehicle
 {
+// TODO: Add l-value and r-value constructors.
+// TODO: Add a templated constructor for specific use cases.
+// TODO: Add a destructor that cleans up the vtable member by destroying the concrete instance.
 };
 
+//The classes Car and Bus should not be changed for these excercises. Only vehicle has to be changed.
 struct Car
 {
+  std::string id() { return m_id; }
   std::string m_id;
+  int m_int = 1;
 };
 
 struct Bus
 {
+  std::string id() { return m_id; }
   std::string m_id;
+  int m_int = 2;
 };
 
 /**
- * Uncomment the following code and make it work
+ * GOAL: Experiment with external inheritance by extending the Vehicle class.
+ * Compare the effectiveness of value semantics with internal versus external inheritance to determine which works most intuitivly.
  */
 void test_1()
 {
+// TODO: Uncomment the asserts below and fix the TODOs in the Vehicle class.
+// The copy constructor is not needed for this exercise yet.
+
 /*
   std::vector<Vehicle> vs;
   vs.emplace_back(Car{"car_1"});
@@ -35,12 +47,13 @@ void test_1()
   ASSERT(vs[3].id() == "bus_2");
 */
 }
-
 /**
- * Uncomment the following code and make it work
+ * GOAL: Continuation of test_1. Split test_1 and test_2 to minimize the changes needed to the Vehicle class in a single test.
  */
 void test_2()
 {
+  // TODO: Uncomment the asserts below. The copy constructor will be required 
+  // for this exercise.
 /*
   std::vector<Vehicle> v_old;
   v_old.emplace_back(Car{"car_1"});
@@ -57,13 +70,14 @@ void test_2()
 }
 
 /**
- * Can you also provide access to member variables iso functions?
- * Add a int member to Car and Bus and provide access to it via Vehicle
- * Uncomment the following code and make it work
+ * GOAL: Provide direct access to member variables rather than through accessor functions.
+ * Extend the vtable implementation to expose direct access to the internal m_id member.
  */
 void test_3()
 {
-/*
+  // TODO: Add a method to the vtable that returns an l-value reference to an int.
+  // uncomment the asserts.
+ /*
   std::vector<Vehicle> v;
   v.emplace_back(Car{"car_1"});
   v.emplace_back(Bus{"bus_1"});
@@ -72,13 +86,13 @@ void test_3()
   ASSERT(v[0].m_int == 1);
   ASSERT(v[1].id() == "bus_1");
   ASSERT(v[1].m_int == 2);
-*/
+ */
 }
 
 
 }
  
-void values_ex4()
+void values_ex2()
 {
   test_1();
   test_2();
