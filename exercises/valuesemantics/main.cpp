94cc9c5009776e2098f810b61a6f90b03e699500
William <william.nagels@gmail.com>
2024-09-16 14:52:21 +0200

    Update main.cpp

--- exercises/valuesemantics/main.cpp
+++ exercises/valuesemantics/main.cpp
@@ -3,14 +3,10 @@
 void values_ex1();
 void values_ex2();
 void values_ex3();
-void values_ex4();
-void values_ex5();
 
 int main()
 {
   values_ex1();
   values_ex2();
   values_ex3();
-  values_ex4();
-  values_ex5();
 }
