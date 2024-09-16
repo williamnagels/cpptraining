05192db0dcd58bc17a309af0cfcd1b102b4b2fa7
William <william.nagels@gmail.com>
2024-09-16 18:12:21 +0200

    Update ex1.cpp

--- exercises/strongtypes/ex1.cpp
+++ exercises/strongtypes/ex1.cpp
@@ -1,65 +1,70 @@
-#include <iostream>
-#include <string>
-
+#include <global.h>
+namespace
+{
 // Define the Command struct
-struct Command {
-    Command(const char* command)
-        : m_command(command), m_endIterator(m_command.end()) {}
-    
-    Command(const std::string& command)
-        : m_command(command), m_endIterator(m_command.end()) {}
-    
-    void setMayPrintArguments(bool printArguments) {
-        m_mayPrintArguments = printArguments;
+struct Command
+{
+    //TODO: create a templated constructor which takes one argument 'input' that is only part of overload resolution
+    //when 'input' can be used to construct an std::string. Throw a readable compiler error in 
+    //all other cases. Initialize m_command with the value of 'input'. call setMayPrintArguments in the body of this constructor.
+
+    // Override the default printing behaviour.
+    //TODO: when printArguments is false, look for the start of the first argument (first occurance of a space ' ') and 
+    //update the end iterator class member 'm_endIterator'.
+    void setMayPrintArguments(bool printArguments) 
+    {
     }
     
-    std::string::const_iterator begin() const {
+    std::string::const_iterator begin() const 
+    {
         return m_command.begin();
     }
     
-    std::string::const_iterator end() const {
-        return m_command.end();
+    std::string::const_iterator end() const 
+    {
+        return m_endIterator;
     }
     
-    const char* getUnsafeCString() const {
-        return m_command.c_str();
+    //TODO: Implement this function so that it writes the command 'm_command' to 
+    //the outputstream os. Respect m_endIterator set by setMayPrintArguments.
+    void printCommand(std::ostream& os) const 
+    {
     }
-    
-    // Print command based on the flag
-    void printCommand(std::ostream& os) const {
-        if (m_mayPrintArguments) {
-            os << "Command: " << m_command;
-        } else {
-            os << "Arguments are hidden.";
-        }
-    }
-
 private:
     std::string m_command;
     std::string::const_iterator m_endIterator;
-    bool m_mayPrintArguments = false;
-
     // Friend function to enable ostream overload
     friend std::ostream& operator<<(std::ostream& os, const Command& cmd);
 };
 
-// Define the ostream overload
-std::ostream& operator<<(std::ostream& os, const Command& cmd) {
+// ostream command overload definition, No changes needed
+std::ostream& operator<<(std::ostream& os, const Command& cmd)
+{
     cmd.printCommand(os);
     return os;
 }
 
-int main() {
-    // Create a Command object with a sample command
-    Command cmd("run --option value --secret password123");
-    
-    // Test when printing arguments is not allowed
-    cmd.setMayPrintArguments(false);
-    std::cout << cmd << std::endl; // Should print "Arguments are hidden."
+// GOAL: The goal of this exercise is to demonstrate how wrapping an std::string
+// in a custom command class can help protect sensitive data.
+// Passing a boolean like `bSensitive` alongside a string is risky because the 
+// relationship between the two is not enforced by the type system. 
+// Another developer might not realize that the boolean and the string are intended 
+// to be used together, leading to potential misuse or security vulnerabilities.
+void test_1()
+{
+    Command command("run --option value --secret williamIsAwesome");
 
-    // Test when printing arguments is allowed
-    cmd.setMayPrintArguments(true);
-    std::cout << cmd << std::endl; // Should print "Command: run --option value --secret password123"
-
-    return 0;
+    //TODO uncomment the asserts when the command class has been implemented.
+    std::stringstream ss;
+    ss << command;
+    // ASSERT(ss.str() == "run");
+    ss.clear();
+    command.setMayPrintArguments(true);
+    ss << command;
+    // ASSERT(ss.str() == "run --option value --secret williamIsAwesome");
+}
+}
+void type_safe_ex1()
+{
+  test_1();
 }
