4ef00dd2be10fec3b2542c3475d90446424b6848
William <william.nagels@gmail.com>
2024-09-17 14:47:19 +0200

    Update ex3.cpp

--- exercises/containers/ex3.cpp
+++ exercises/containers/ex3.cpp
@@ -2,19 +2,18 @@
 
 namespace
 {
-//GOAL: Filter out all uneven integers from container 'input' convert
+//GOAL: Filter out all uneven integers from container 'input', convert
 //the remaining integers to instances of type std::string and append them 
 //to container 'output'.
 //
-//Use an additional temporary container if you must, but it is possible to 
-//do this using 1 algorithm from https://en.cppreference.com/w/cpp/algorithm
+//It is possible to do this using 1 algorithm from https://en.cppreference.com/w/cpp/algorithm
 void test_1()
 {
   std::vector<int> input = {4,5,6,8,9};
   std::vector<std::string> output;
-  //TODO: filter out uneven elements from container 'input' and push the result to vector 'output'
-  // ASSERT(output[1]  == "6");
-  // ASSERT(std::size(input)  == 5);
+  //TODO: Filter out uneven elements from container 'input' and push the remaining vector to 'output'
+  //ASSERT(output[1]  == "6");
+  //ASSERT(std::size(input)  == 5);
 }
 }
  
