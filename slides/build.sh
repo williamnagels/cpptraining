26e2ebff18963cd6ecda21175d5a10625d0f863d
William <william.nagels@gmail.com>
2024-09-23 17:02:29 +0200

    Delete slides/build.sh

--- slides/build.sh
+++
@@ -1,3 +0,0 @@
-mkdir build                                                                                            
-pdflatex --output-directory=build 1_intro.tex
-find build -type f ! -name "*.pdf" -delete
