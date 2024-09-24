279324fb92c93040514060a02767e3aa466876a6
William <william.nagels@gmail.com>
2024-09-24 17:08:47 +0200

    Update ex1.cpp

--- exercises/compile_time_polymorphism/ex1.cpp
+++ exercises/compile_time_polymorphism/ex1.cpp
@@ -1,8 +1,8 @@
 #include <global.h>
 namespace
 {
-//TODO: add a printHelper template specialization that is only instantiated when
-//the type to print is a pointer
+//TODO: Add a printHelper partial template specialization where T is a pointer type.
+//This primary template candidate is OK and should not be changed.
 template <typename T, bool>
 struct PrintHelper<T>
 {
@@ -15,33 +15,27 @@ struct PrintHelper<T>
 template <typename T>
 void print(T const& t, std::ostream& os)
 {
-    //TODO: Instantiate another PrintHelper if T is a pointer type.
-    //TIP: check the list of possibilities at https://en.cppreference.com/w/cpp/meta
+    //TODO: Instantiate a different PrintHelper if T is a pointer type.
+    //TIP: C++ lib has built-in type checks to see if a type T is a poiner-like type. Take a look at https://en.cppreference.com/w/cpp/meta
     PrintHelper<T, true>::print(t, os);
 }
-//GOAL: The goal of this exercise is to get some experience with template overloads.
-//The idea is that some print function takes either a value or a pointer type. it prints the
-//the value of the value type and print the value to which the pointer refers.
-//To make this work the print() method should instantiate the correct PrintHelper type.
-//A new printHelper type needs to be added to work with pointers only. it does not exist right now.
+
+//GOAL: The goal of this exercise is to get some experience with template overloading and template specialization.
+//The function print below is called for a value-type 'int' in the uncommented code.
+//it will write the value of i to the provided std::stringstream.
+//The idea is that in the commented call to print a pointer is passed which selects another candidate because it is a pointer type.
 void test_1()
 {
     std::stringstream ss;
     int i = 13;
     print(i, ss);
     ASSERT(ss.str() == "13");
-    //TODO: uncomment the asserts below when the pointer overload has been implemented
+    //TODO: uncomment the asserts below when the pointer-like PrintHelper has been implemented
     /*ss.clear();
     print(&i, ss);
     ASSERT(ss.str() == "pointer with value 13");*/
 }
-//TODO: private another overload for a pointer type T, check the output
-//of the print in the actual test_2 function. This function is OK and
-//should not be changed.
-static void print_impl(T const& t, std::ostream& os, std::true_type)
-{
-    os << t;
-}
+
 template <typename T>
 void print2(T const& t, std::ostream& os)
 {
@@ -49,15 +43,14 @@ void print2(T const& t, std::ostream& os)
     //the asserted output in test_2 correctly if T is a pointer.
     os << t;
 }
-//GOAL: Solve test_1 with a compile-time if instead of template overloads
-//only print2 should be changed for this.
+//GOAL: Solve test_1 with a compile-time if expression instead of overloading the function print2.
 void test_2()
 {
     std::stringstream ss;
     int i = 13;
     print2(i, ss);
     ASSERT(ss.str() == "13");
-    //TODO: uncomment the asserts below when the compiletime if has been added.
+    //TODO: uncomment the asserts below when the compile-time if logic has been added to print2
     /*ss.clear();
     print2(&i, ss);
     ASSERT(ss.str() == "pointer with value 13");*/
